4c384da14287af1ccdf36f2ff32aa086f812651f
dvny <danaildimitrov2003@gmail.com>
2023-05-09 17:25:00 +0300

    added the carView to loadView() and changed the background colour to mint

--- MVC Car/Controller/ViewController.swift
+++ MVC Car/Controller/ViewController.swift
@@ -8,28 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-        let carView = CarView()
-        var car = cars[1]
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            view.addSubview(carView)
-            carView.translatesAutoresizingMaskIntoConstraints = false
-            NSLayoutConstraint.activate([
-                carView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                carView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                carView.topAnchor.constraint(equalTo: view.topAnchor),
-                carView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            ])
-        }
-        
-        override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-                carView.configure(with: car)
-        }
-
-
+    
+    let carView = CarView()
+    
+    var car = cars[1]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func loadView() {
+        super.loadView()
+        view = carView
+        view.backgroundColor = .systemMint
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        carView.configure(with: car)
+    }
+    
+    
 }
 
